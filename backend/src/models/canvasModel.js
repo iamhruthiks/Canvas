027768735021022d65b74047fc351a0efd5ffde6
iamhruthiks <iamhruthiks@gmail.com>
2025-08-08 21:31:49 +0530

    [backend] add canavs model

---
+++ backend/src/models/canvasModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const elementSchema = new mongoose.Schema({
+  type: {
+    type: String,
+    enum: ["rectangle", "circle", "text", "image"],
+    required: true,
+  },
+  props: {
+    type: Object,
+    required: true,
+  },
+});
+
+const canvasSchema = new mongoose.Schema(
+  {
+    width: { type: Number, required: true },
+    height: { type: Number, required: true },
+    elements: [elementSchema],
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Canvas", canvasSchema);
