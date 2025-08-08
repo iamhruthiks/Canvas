737aaffa5f8d24da06a6cf3b4f0cc50a01bc3caf
iamhruthiks <iamhruthiks@gmail.com>
2025-08-08 19:06:56 +0530

    [backend] setup mongo DB connection

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,14 @@
+require("dotenv").config();
+
 const express = require("express");
 const cors = require("cors");
+const connectDB = require("./src/config/db");
 const canvasRoutes = require("./src/routes/canvasRoutes");
 
 const app = express();
+
+// Connect to MongoDB
+connectDB();
 
 app.use(cors());
 app.use(express.json());
